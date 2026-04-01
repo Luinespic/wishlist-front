5b1493d03be7464382e4393a6bfe863cd553f0ac
Luinespic <lucia_49_92@hotmail.com>
2026-04-01 11:28:50 +0200

    feat: pagina 404

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,3 +1,11 @@
+import { Link } from 'react-router-dom'
+
 export default function NotFoundPage() {
-  return <div>404 Not Found</div>
+  return (
+    <div>
+      <h1>¡Ups! Esta página no existe</h1>
+      <p>Puede que el enlace esté roto o que la página haya sido eliminada.</p>
+      <Link to="/dashboard">Volver al inicio</Link>
+    </div>
+  )
 }
